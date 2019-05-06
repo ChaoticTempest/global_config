db35592d9549adc89739794b58d92e66a1409869
Phuong N <luckyadvancement@gmail.com>
2019-05-05 18:57:09 -0700

    Simplified cfg and added 'default' for cfg parsing

--- cfg.py
+++ cfg.py
@@ -1,9 +1,13 @@
 import sys
 import yaml
 
+EMPTYSET = set()
+
 __config_data = None
 __config_usage = None
-__config_graph = None
+__config_enabled = None
+__config_defaults_enabled = None
+
 
 
 def crawl(name, deps, graph, seen=None):
@@ -36,21 +40,27 @@ def parse_deps(deps, usage):
 
     deptable = dict()
     for category, deplist in usage.items():
-
-        # result = crawl(name=dep, deps=deps[category][dep], graph=deps[category])
-        # print(deps[category])
         result = crawl(name=category, deps=deplist, graph=deps[category])
         deptable[category] = result
 
     return deptable
 
 
+def find_defaults(deflist):
+    usages = dict()
+    for category, deps in deflist.items():
+        if 'default'  in deps:
+            usages[category] = deps['default']
+    return usages
+
+
 def load_global_config(definitions, usage):
     global __config_data
     global __config_usage
-    global __config_graph
+    global __config_defaults_enabled
+    global __config_enabled
 
-    assert __config_data is None, "Cannot reload global config"
+    assert __config_data is None, "Reloading global config disallowed"
 
     with open(definitions, 'r') as stream:
         __config_data = yaml.load(stream)
@@ -58,7 +68,9 @@ def load_global_config(definitions, usage):
     with open(usage, 'r') as stream:
         __config_usage = yaml.load(stream)
 
-    __config_graph = parse_deps(__config_data, __config_usage)
+    default_usages = find_defaults(__config_data)
+    __config_defaults_enabled = parse_deps(__config_data, default_usages)
+    __config_enabled = parse_deps(__config_data, __config_usage)
 
 
 def fmt(s, *args, **kwargs):
@@ -75,20 +87,23 @@ def cfg(**configs):
         callframe.f_locals[new_name] = fn
 
         # TODO: temprorary. Only getting the first item:
-        name, value = next(iter(configs.items()))
+        category, option = next(iter(configs.items()))
 
-        # print(name in __config_graph.keys())
-        assert name in __config_graph.keys(), "Could not find config: {}".format(name)
+        # Handle default enabled:
+        enabled = __config_enabled
+        if category not in __config_enabled:
+            enabled = __config_defaults_enabled
 
-        # feature="x":  x is not enabled
-        if value not in __config_graph.get(name, set()):
-            if original_name in callframe.f_locals:
-                # return the original function
-                return callframe.f_locals[original_name]
-            else:
-                # Not a valid function: return a function that throws on call
-                return lambda *args, **kwargs: (_ for _ in ()).throw(
-                    Exception(fmt("Cannot call cfg({}='{}') invalidated function `{}`", name, value, original_name)))
+        # feature="x":  x is enabled
+        if option in enabled.get(category, set()):
+            return fn
 
-        return fn
+        # Feature is not enabled. Returned the cached function instead:
+        if original_name in callframe.f_locals:
+            return callframe.f_locals[original_name]
+
+        # Not a valid function: return a function that throws on call:
+        return lambda *args, **kwargs: (_ for _ in ()).throw(
+            Exception(fmt("Cannot call cfg({}='{}') invalidated function `{}`", category, option, original_name)))
+
     return inner
